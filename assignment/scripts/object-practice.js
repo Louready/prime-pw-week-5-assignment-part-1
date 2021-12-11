9989f362312c8480d6306e592c4aadf8cd3bd982
Lou Coughlin <coughlinlou@gmail.com>
2021-12-10 20:58:32 -0600

    firstdraft

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,32 +10,46 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'Lou',
+  lastName: 'Coughlin',
+  hasSiblings: true,
+  shoeCount: 3,
+  favThreeFoods: ['chicken', 'beef', 'tacos'],
+
 };
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
-  - Create a variable called fullName 
-  - Use the firstName & lastName properties of the object you 
-    created above to set its value. 
+  - Create a variable called fullName
+  - Use the firstName & lastName properties of the object you
+    created above to set its value.
   - Console.log fullName
 */
-
+let fullName = me.firstName + me.lastName;
+console.log ('Hi I am' , fullName);
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
-  - Console.log your last favorite food 
+  - Console.log your last favorite food
 */
-
+console.log (me.favThreeFoods[0]);
+console.log (me.favThreeFoods[2]);
 
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
-  - Console.log your updated shoe count. 
+  - Console.log your updated shoe count.
 */
+//console.log (me.shoeCount);
+//function addItem(item) {
+//  me.shoeCount.push(item);
+//  return true;
+//}
 
+//console.log ( addItem(1));
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +57,14 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+
+me.favoriteColor = "grey";
+
+console.log (me);
+
+function doit(x) {
+
+return true;
+}
+console.log (doit(1,2,3));
